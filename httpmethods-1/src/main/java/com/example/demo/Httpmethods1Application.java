12846d8fe9c4aa2d6dda05b82938d7fc27c81986
truesparksoftware@gmail.com <jeevanbarguje1@gmail.com>
2022-10-31 08:20:38 +0530

    added code for http method basic

---
+++ httpmethods-1/src/main/java/com/example/demo/Httpmethods1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Httpmethods1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Httpmethods1Application.class, args);
+	}
+
+}
